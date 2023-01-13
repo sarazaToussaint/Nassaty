ef462e69f6eea4a29b9585cb44cf83c3c66a5d89
sarazaToussaint <tmsaraza@gmail.com>
2023-01-13 16:19:09 +0200

    Fix linter errors

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
 
-export const Contact = () => {
-  return (
-    <div>
-      <nav>
-       <h1>Logo</h1>
-       <ul>
-         <li>
-           <Link to= "/">Home</Link>
-         </li>
-         <li>
-           <Link to= "/Project">Project</Link>
-         </li>
-        </ul>
-      </nav>
- 
- <di className = "home-body">
-   <h1>CONTACT</h1>
- </di>
-    </div>
-  )
-}
+const Contact = () => (
+  <div>
+    <nav>
+      <h1>Logo</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/Project">Project</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <di className="home-body">
+      <h1>CONTACT</h1>
+    </di>
+  </div>
+);
+
+export default Contact;
