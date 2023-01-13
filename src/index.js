907b493d622c4e78e1c1ae27214890cde42d1705
sarazaToussaint <tmsaraza@gmail.com>
2023-01-13 16:19:02 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-     <App />
-    </Router> 
-  </React.StrictMode>
+      <App />
+    </Router>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
