712845d1115ca88d80e1b9a2863938008301a20d
sarazaToussaint <tmsaraza@gmail.com>
2023-01-13 16:19:15 +0200

    Fix linter errors

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
 
-export const Home = () => {
-  return (
-    <div>
+const Home = () => (
+  <div>
     <nav>
-    <h1>Logo</h1>
-    <ul>
-     <li>
-       <Link to= "/Project">Pojects</Link>
-     </li>
-     <li>
-       <Link to= "/Contact">Contact</Link>
-     </li>
-    </ul>
- </nav>
- 
- <di className = "home-body">
-   <h1>NASSATY</h1>
- </di>
-</div>
-  )
-}
+      <h1>Logo</h1>
+      <ul>
+        <li>
+          <Link to="/Project">Pojects</Link>
+        </li>
+        <li>
+          <Link to="/Contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <di className="home-body">
+      <h1>NASSATY</h1>
+    </di>
+  </div>
+);
+
+export default Home;
