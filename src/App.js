1452caca2ae04e155f42a36d07e5c582e7807b02
sarazaToussaint <tmsaraza@gmail.com>
2023-01-13 16:18:57 +0200

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from "./component/Home";
-import { Project } from "./component/Project";
-import { Contact } from "./component/Contact";
-
+import Home from './component/Home';
+import Project from './component/Project';
+import Contact from './component/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Project" element={<Project />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
-    </div>  
+    </>
   );
 }
 
