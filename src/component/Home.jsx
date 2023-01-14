43a97bd12bdcb42ee01c31f447043744b499f0d0
sarazaToussaint <tmsaraza@gmail.com>
2023-01-14 15:25:45 +0200

    Fetch data from json file

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { NavLink as Link } from 'react-router-dom';
+import data from './database/data.json';
+import styles from './Home.module.css';
 
 const Home = () => (
-  <div>
+  <div className={styles.homeContainer}>
     <nav>
-      <h1>Logo</h1>
+      <h1>
+        <img className={styles.logo} src={data.data.logo} alt={data.data.projectName} />
+      </h1>
       <ul>
         <li>
-          <Link to="/Project">Pojects</Link>
+          <Link to="/Project">PROJECTS</Link>
         </li>
         <li>
-          <Link to="/Contact">Contact</Link>
+          <Link to="/Contact">CONTACT</Link>
         </li>
       </ul>
     </nav>
 
     <di className="home-body">
-      <h1>NASSATY</h1>
+      <h1>{data.data.projectName}</h1>
     </di>
   </div>
 );
