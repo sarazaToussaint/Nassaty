0319489b364f2a8f6259b93b3077caadb65639fb
sarazaToussaint <tmsaraza@gmail.com>
2023-01-20 21:13:17 +0200

    Make some changes

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => (
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
           Maiores voluptatibus excepturi eaque doloribus dignissimos
           doloremque, odit impedit aut laudantium reiciendis veritatis
-          sit incidunt. Perspiciatis culpa cumque, ratione ipsum eaque illo!
+          sit incidunt. Perspiciatis culpa cumque, ipsum eaque illo!
         </p>
         <div className={classes.address}>
           <h3>Address</h3>
