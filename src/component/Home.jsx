f900ad34ac2897d8aa60204799da5e05cf688fae
sarazaToussaint <tmsaraza@gmail.com>
2023-01-24 13:14:53 +0200

    Fix some issues on the home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -53,12 +53,12 @@ const Home = () => (
       style={{
         backgroundImage: `url(${`${process.env.PUBLIC_URL}/assets/background.png`})`,
         opacity: '8%',
-        // position: 'absolute',
+        position: 'absolute',
         width: '100%',
         height: '100%',
-        bottom: '0',
-        left: '0',
-        right: '0',
+        // bottom: '0',
+        // left: '0',
+        // right: '0',
       }}
       className={styles.bgImage}
     />
