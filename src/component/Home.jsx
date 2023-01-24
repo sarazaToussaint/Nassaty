e355d784c7413e2635ca61b9d16f22e137f3046f
sarazaToussaint <tmsaraza@gmail.com>
2023-01-24 21:35:01 +0200

    Make some changies

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,101 +1,114 @@
-import React from 'react';
+import { useState, React } from 'react';
 import { NavLink as Link } from 'react-router-dom';
 import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 import data from './database/data.json';
 import styles from './Home.module.css';
 
-const Home = () => (
-  <div className={styles.homeContainer}>
-    <nav>
-      <h1>
-        <img className={styles.logo} src={data.data.logo} alt={data.data.projectName} />
-      </h1>
-      <button type="button" className="hamburger">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          viewBox="0 0 20 20"
-          fill="white"
+const Home = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  return (
+    <div className={styles.homeContainer}>
+      <nav>
+        <h1>
+          <img className={styles.logo} src={data.data.logo} alt={data.data.projectName} />
+        </h1>
+        <button
+          type="button"
+          className="hamburger"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
         >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            viewBox="0 0 20 20"
+            fill="white"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
 
-      <ul>
-        <li>
-          <Link to="/Project">PROJECTS</Link>
-        </li>
-        <li>
-          <Link to="/Contact">CONTACT</Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className={
+          isNavExpanded ? 'nav expanded' : 'nav'
+        }
+        >
+          <li>
+            <Link to="/Project">PROJECTS</Link>
+          </li>
+          <li>
+            <Link to="/Contact">CONTACT</Link>
+          </li>
+        </ul>
+      </nav>
 
-    <section className={styles.homeBody}>
+      <section className={styles.homeBody}>
+        <div
+          style={{
+            backgroundImage: `url(${`${process.env.PUBLIC_URL}/assets/cercleImg.png`})`,
+            opacity: '3%',
+            position: 'absolute',
+            width: '600px',
+            height: '600px',
+          }}
+          className={styles.bgImage2}
+        />
+        <div className={styles.sideLeft}>
+          <div className={styles.leftLine} />
+          <div>
+            <img className={styles.left} src={data.data.sideImg} alt={data.data.projectName} />
+          </div>
+          <div className={styles.leftLine} />
+        </div>
+        <div className={styles.nameBody}>
+          <h1>{data.data.projectName}</h1>
+        </div>
+        <div className={styles.sideRight}>
+          <div className={styles.rightLine} />
+          <div>
+            <img className={styles.right} src={data.data.sideImg} alt={data.data.projectName} />
+          </div>
+          <div className={styles.rightLine} />
+        </div>
+      </section>
       <div
         style={{
-          backgroundImage: `url(${`${process.env.PUBLIC_URL}/assets/cercleImg.png`})`,
-          opacity: '3%',
+          backgroundImage: `url(${`${process.env.PUBLIC_URL}/assets/background.png`})`,
+          opacity: '8%',
           position: 'absolute',
-          width: '600px',
-          height: '600px',
+          width: '100%',
+          height: '100%',
+          bottom: '0',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
         }}
-        className={styles.bgImage2}
+        className={styles.bgImage}
       />
-      <div className={styles.sideLeft}>
-        <div className={styles.leftLine} />
-        <div>
-          <img className={styles.left} src={data.data.sideImg} alt={data.data.projectName} />
+      <footer>
+        <div className={styles.socialMedia}>
+          <a href="prince" target="_blank" rel="noreferrer">
+            <FaFacebookF />
+          </a>
+          <a href="prince" target="_blank" rel="noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="prince" target="_blank" rel="noreferrer">
+            <FaInstagram />
+          </a>
         </div>
-        <div className={styles.leftLine} />
-      </div>
-      <div className={styles.nameBody}>
-        <h1>{data.data.projectName}</h1>
-      </div>
-      <div className={styles.sideRight}>
-        <div className={styles.rightLine} />
-        <div>
-          <img className={styles.right} src={data.data.sideImg} alt={data.data.projectName} />
+        <div className={styles.scroll}>
+          <div className={styles.text}>Scroll</div>
+          <div className={styles.footLine} />
         </div>
-        <div className={styles.rightLine} />
-      </div>
-    </section>
-    <div
-      style={{
-        backgroundImage: `url(${`${process.env.PUBLIC_URL}/assets/background.png`})`,
-        opacity: '8%',
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        bottom: '0',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-      }}
-      className={styles.bgImage}
-    />
-    <footer>
-      <div className={styles.socialMedia}>
-        <a href="prince" target="_blank" rel="noreferrer">
-          <FaFacebookF />
-        </a>
-        <a href="prince" target="_blank" rel="noreferrer">
-          <FaTwitter />
-        </a>
-        <a href="prince" target="_blank" rel="noreferrer">
-          <FaInstagram />
-        </a>
-      </div>
-      <div className={styles.scroll}>
-        <div className={styles.text}>Scroll</div>
-        <div className={styles.footLine} />
-      </div>
-    </footer>
-  </div>
-);
+      </footer>
+    </div>
+  );
+};
 
 export default Home;
