de5ce0fcdce25d9ebf28a171c9c1269d90ee8457
sarazaToussaint <tmsaraza@gmail.com>
2023-01-24 13:35:45 +0200

    Make changes to the bg image on the home page

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -56,9 +56,7 @@ const Home = () => (
         position: 'absolute',
         width: '100%',
         height: '100%',
-        // bottom: '0',
-        // left: '0',
-        // right: '0',
+        bottom: '0',
       }}
       className={styles.bgImage}
     />
