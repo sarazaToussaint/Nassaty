bc026c6a8b3112f0c227c187e56866d1918b4071
sarazaToussaint <tmsaraza@gmail.com>
2023-02-12 10:09:11 +0200

    Make some change on the contact page mobile v

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -15,6 +15,7 @@ const Contact = () => {
         width: '100%',
         height: '100%',
         bottom: '0',
+        opacity: '70%',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
